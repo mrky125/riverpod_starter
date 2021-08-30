a9abe203b59ef25b83571f607545fdea1d233777
mkry125 <4ex21ia@gmail.com>
2021-08-30 23:12:57 +0900

    Add helloworld sample app by riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ProviderScope(
+    child: HelloApp(),
+  ));
 }
 
+// This redundant writing is below.
+// final helloWorldProvider = Provider((ref) {
+//   return 'Hello World';
+// });
+final helloProvider = Provider((ref) => 'Hello World');
+
+class HelloApp extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final helloWorld = ref.watch(helloProvider);
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(helloWorld),
+        ),
+      ),
+    );
+  }
+}
+
+
+///
+/// Initial default app.
+///
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
