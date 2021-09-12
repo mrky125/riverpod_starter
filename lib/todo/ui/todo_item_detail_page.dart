40447bf6c4120bd40fbc019d48c4a8ef66fe6f87
mkry125 <4ex21ia@gmail.com>
2021-09-13 02:16:16 +0900

    Add detail page (not implemented)

---
+++ lib/todo/ui/todo_item_detail_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:riverpod_starter/todo/entity/todo_item.dart';
+
+class TodoItemDetailPage extends StatelessWidget {
+  const TodoItemDetailPage({this.todoItem});
+  final TodoItem? todoItem;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
