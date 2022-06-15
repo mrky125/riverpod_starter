8bd78e4453b5922998a75e6d1d073cb629c81847
mrky125 <4ex21ia@gmail.com>
2022-06-15 12:08:04 +0900

    test diff

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_starter/inherited_page.dart';
 
+/// test
 void main() {
   // runApp(ProviderScope(
   //   child: CounterApp(),
